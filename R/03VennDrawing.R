989233999fb1c4bf7fd61df24c03dfa60dbdd43c
Jeremy Bowyer <JeremyBowyer@gmail.com>
2019-07-01 16:11:02 -0400

    Update 03VennDrawing.R

--- R/03VennDrawing.R
+++ R/03VennDrawing.R
@@ -375,6 +375,9 @@ makevp.eqsc <- function(xrange,yrange,centres) {
 	# cf Fig 7.4 of Murrell R Graphics
 	pushViewport(plotViewport(name="Vennmar",c(1,1,1,1)))
 	pushViewport(viewport(name="Vennlay",layout=grid.layout(1,1,widths=diff(xrange),heights=diff(yrange),respect=TRUE)))
+	pars = par("mar")
+	pars[2] = par[2] + 2
+	par(mar = pars)
 	pushViewport(viewport(name="Vennvp",layout.pos.row=1,layout.pos.col=1,xscale=xrange,yscale=yrange))
 	}
 
