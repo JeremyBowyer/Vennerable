8ab6982c55a200063e00c0b94770530fafc21b09
Jeremy Bowyer <JeremyBowyer@gmail.com>
2019-07-02 19:18:08 -0400

    Update 03VennDrawing.R

--- R/03VennDrawing.R
+++ R/03VennDrawing.R
@@ -359,7 +359,7 @@ PlotSetLabels <- function(object,gp) {
 	for (ij in 1:nrow(VLabels)) {
 		#grid.text(x=VLabels$x[ij],y=VLabels$y[ij],hjust=hj[ij],
 		#vjust=vj[ij],gp=gp[[ij]],label=as.character(VLabels$Label[ij]),default.units="native")
-		grid.segments(x0=VLabels$x[ij],x1=VLabels$x[ij],y0=c(0),y1=VLabels$y[ij]))
+		grid.segments(x0=VLabels$x[ij],x1=VLabels$x[ij],y0=0,y1=VLabels$y[ij])
 		grid.text(x=VLabels$x[ij],y=0,label=as.character(VLabels$Label[ij]))
 	}
 }
